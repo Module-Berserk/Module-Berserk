679321dcbaac51a8a14f47d8a97580727e6376fd
ownfos <mejhg1234@gmail.com>
2024-05-28 21:30:11 +0900

    Make elevator automatically return to the initial position

--- ModuleBerserk/Assets/Scripts/Environment/Elevator.cs
+++ ModuleBerserk/Assets/Scripts/Environment/Elevator.cs
@@ -2,27 +2,29 @@ using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Assertions;
 
+// 활성화되는 순간 꼭대기까지 올라갔다가 다시 돌아오는 엘리베이터.
+// 이미 시작된 이동은 취소할 수 없다.
 [RequireComponent(typeof(Rigidbody2D))]
 public class Elevator : MonoBehaviour
 {
+    // 현재 위치에서 얼마나 높이 올라갈지
     [SerializeField] private float movementRange;
-    [SerializeField] private float delayBeforeMovement;
+    // 이동 도중 부여할 속도
+    [SerializeField] private float movementSpeed;
+    // 상승을 시작하기 전에 잠깐 기다리며 이펙트를 보여주는 시간
+    [SerializeField] private float initialMovementDelay = 1f;
+    // 꼭대기에 도달한 뒤 다시 아래로 내려오기 전에 기다리는 시간
+    [SerializeField] private float delayBeforeReturning = 3f;
 
-    [Serializable]
-    private enum State
-    {
-        Up,
-        Down,
-    }
-    [SerializeField] private State initialState = State.Down;
+    // 이동을 멈추기 위한 목적지와의 거리 조건
+    private const float MOVEMENT_STOP_DISTANCE_THRESHOLD = 0.1f;
 
     private Rigidbody2D rb;
     private float heightUpperBound;
     private float heightLowerBound;
-
-    private CancellationTokenSource movementCancellation = new();
-    private bool isMoving = false;
+    private bool isActive = false;
 
     private void Awake()
     {
@@ -34,85 +36,68 @@ public class Elevator : MonoBehaviour
     // 엘리베이터의 y축 이동 범위를 계산함
     private void CalculateMovementBoundary()
     {
-        float initialHeight = rb.position.y;
-        if (initialState == State.Down)
-        {
-            heightUpperBound = initialHeight + movementRange;
-            heightLowerBound = initialHeight;
-        }
-        else
-        {
-            heightUpperBound = initialHeight;
-            heightLowerBound = initialHeight - movementRange;
-        }
+        heightLowerBound = rb.position.y;
+        heightUpperBound = heightLowerBound + movementRange;
     }
 
-    public void StartMovingUp()
+    public void ActivateElevator()
     {
-        StartMovement(heightUpperBound).Forget();
-    }
-    
-    public void StartMovingDown()
-    {
-        StartMovement(heightLowerBound).Forget();
-    }
-
-    private async UniTask StartMovement(float destinationHeight)
-    {
-        // 이번 task에 사용할 토큰.
-        // CancellationTokenSource는 취소할 때마다 새로
-        // 생성되므로 레퍼런스가 일관적이지 않음
-        CancellationToken cancellationToken = movementCancellation.Token;
-        
-        // 이미 이동 중이었다면 모션 취소
-        if (isMoving)
+        // 이미 이동 중이라면 요청 무시
+        if (isActive)
         {
-            CancelMovement();
-            cancellationToken = movementCancellation.Token; // 새 토큰으로 교체
-        }
-        // 정지 상태였다면 잠깐 대기한 뒤 이동 시작
-        //
-        // Note:
-        // 플레이어가 엘레베이터를 레버 등으로 작동한 뒤
-        // 탑승하러 오기까지 약간의 시간적 여유가 필요함
-        else
-        {
-            PlayerElevatorMoveStartEffect();
-
-            await UniTask.WaitForSeconds(delayBeforeMovement, cancellationToken: cancellationToken);
-            if (cancellationToken.IsCancellationRequested)
-            {
-                return;
-            }
+            return;
         }
 
-        isMoving = true;
+        StartElevatorMovementAsync().Forget();
+    }
 
-        // TODO: 더 복잡한 속도 커브가 필요한 경우 DoTween으로 전환할 것
-        while (true)
+    private async UniTask StartElevatorMovementAsync()
+    {
+        // 엘리베이터 움직임은 비활성화 상태에서만 시작될 수 있음.
+        // 중간에 취소하거나 재시작하는 상황이 일어나면 안됨.
+        Assert.IsFalse(isActive);
+
+        isActive = true;
+
+        // 1. 이펙트 출력하고 잠시 기다린다
+        PlayerElevatorMoveStartEffect();
+        await UniTask.WaitForSeconds(initialMovementDelay);
+
+        // 2. 위로 이동한다
+        await MoveToAsync(heightUpperBound);
+
+        // 3. 플레이어가 내릴 때까지 잠시 기다린다
+        await UniTask.WaitForSeconds(delayBeforeReturning);
+
+        // 4. 다시 원래 자리로 돌아간다
+        await MoveToAsync(heightLowerBound);
+
+        isActive = false;
+    }
+
+    // TODO: 더 복잡한 속도 커브가 필요한 경우 DoTween으로 전환할 것
+    private async UniTask MoveToAsync(float destinationHeight)
+    {
+        // 목적지 방향으로 일정한 속도 부여
+        rb.velocity = (destinationHeight > rb.position.y ? Vector2.up : Vector2.down) * movementSpeed;
+
+        // 이동 속도가 너무 빨라서 목적지를 지나쳐버리지 않는 한
+        // 한 프레임씩 기다리며 거리가 충분히 가까워졌는지 체크
+        while (IsWithinBoundary())
         {
             float heightDiff = destinationHeight - rb.position.y;
-
-            // 아직 목적지와 거리가 있고 목적지를 넘어선 상황도 아니라면 계속 이동
-            if (Mathf.Abs(heightDiff) > 0.1f && IsWithinBoundary())
-            {
-                rb.velocity = Vector2.up * Mathf.Sign(heightDiff);
-
-                await UniTask.NextFrame(cancellationToken: cancellationToken);
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return;
-                }
-            }
-            else
+            bool isCloseEnough = Mathf.Abs(heightDiff) < MOVEMENT_STOP_DISTANCE_THRESHOLD;
+            if (isCloseEnough)
             {
                 break;
             }
+
+            await UniTask.NextFrame();
         }
         
+        // 충분히 가까워진 상태이므로 정확한 도착 좌표로 이동
         rb.position = new Vector2(rb.position.x, destinationHeight);
         rb.velocity = Vector2.zero;
-        isMoving = false;
     }
 
     private bool IsWithinBoundary()
@@ -120,17 +105,9 @@ public class Elevator : MonoBehaviour
         return rb.position.y >= heightLowerBound && rb.position.y <= heightUpperBound;
     }
 
-    private void CancelMovement()
-    {
-        Debug.Log("이동 취소됨!");
-        movementCancellation.Cancel();
-        movementCancellation.Dispose();
-        movementCancellation = new();
-    }
-
     private void PlayerElevatorMoveStartEffect()
     {
-        // TODO: 이제 곧 엘리베이터 움직인다는 효과 재생 ex) "덜그럭" 하는 효과음
+        // TODO: 이제 곧 엘리베이터 움직인다는 효과 재생 ex) "덜그럭" 하는 효과음, 약간의 진동
         Debug.Log("엘리베이터가 곧 움직입니다...");
     }
 }
--- ModuleBerserk/Assets/Scripts/Test/TestLever.cs
+++ ModuleBerserk/Assets/Scripts/Test/TestLever.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 // Trigger와 조건부 상호작용을 테스트하기 위해 만든 클래스.
 // 주변에 적이 없는 경우에만 상호작용을 할 수 있다.
-public class TestLever : ToggleSwitchTrigger, IInteractable
+//
+// TODO: 정식 레버로 사용할 계획이라면 OnActivate와 OnDeactivate에 레버 애니메이션 추가할 것
+public class TestLever : Trigger, IInteractable
 {
     [Header("Required Trigger")]
     [SerializeField] private NoEnemyNearbyTrigger noEnemyNearbyTrigger;
@@ -21,11 +24,24 @@ public class TestLever : ToggleSwitchTrigger, IInteractable
 
     void IInteractable.StartInteraction()
     {
-        Toggle();
+        // 한 번 당기면 1초 뒤에 원상태로 돌아옴
+        ActivateForDurationAsync(1f).Forget();
+    }
+
+    private async UniTask ActivateForDurationAsync(float duration)
+    {
+        Activate();
+        await UniTask.WaitForSeconds(duration);
+        Deactivate();
     }
 
     bool IInteractable.IsInteractionPossible()
     {
-        return noEnemyNearbyTrigger.IsActive;
+        // 조건이 충족되었고 레버가 이미 당겨진 상황이 아니라면 ok
+        //
+        // Note:
+        // 엘리베이터에 사용되는 레버는 당겨도 잠시 뒤에 원상태로 돌아옴.
+        // 플레이어가 작동시켜놓고 엘베 놓치는 경우 다시 작동시킬 수 있도록 하기 위함.
+        return noEnemyNearbyTrigger.IsActive && !IsActive;
     }
 }
