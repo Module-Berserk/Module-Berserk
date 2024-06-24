6c54d6eab4d737c196695b5ad3fcdbc052f59496
ownfos <mejhg1234@gmail.com>
2024-06-24 22:05:55 +0900

    Implement dialogue box interaction with selectable options

--- ModuleBerserk/Assets/Scripts/Test/DialogueBox.cs
+++ ModuleBerserk/Assets/Scripts/Test/DialogueBox.cs
@@ -1,10 +1,10 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Cysharp.Threading.Tasks;
+using UnityEngine.InputSystem;
 
-public class DialogueBox : MonoBehaviour
+public class DialogueBox : MonoBehaviour, IUserInterfaceController
 {
     [SerializeField] private SpriteRenderer backgroundRenderer;
     [SerializeField] private TextMeshPro textMesh;
@@ -14,9 +14,26 @@ public class DialogueBox : MonoBehaviour
     [SerializeField] private float heightOffset = 0f;
     [SerializeField] private float characterAppearanceDelay = 0.02f;
 
+    private bool isSelectingDialogueOption = false;
+    private int selectedDialogueOptionIndex;
+    private List<string> dialogueOptions;
+
     private void Start()
     {
-        TestDialogueSequence().Forget();
+        TestDialogueSelection().Forget();
+        // TestDialogueSequence().Forget();
+    }
+
+    private async UniTask TestDialogueSelection()
+    {
+        List<string> options = new();
+        options.Add("1. 커피를 마신다");
+        options.Add("2. 도넛을 먹는다");
+        options.Add("3. 굶는다");
+
+        int result = await BeginDialogueSelection(options);
+
+        Debug.Log($"최종 선택: {options[result]}");
     }
 
     private async UniTask TestDialogueSequence()
@@ -59,5 +76,79 @@ public class DialogueBox : MonoBehaviour
         // 3. 텍스트와 배경 이미지 모두 중앙에 정렬되므로
         // 말풍선 꼬리가 제자리에 머물도록 약간 오른쪽으로 중심을 옮긴다
         transform.localPosition = backgroundRenderer.size / 2f + Vector2.up * heightOffset;
+    }
+
+    public async UniTask<int> BeginDialogueSelection(List<string> options)
+    {
+        // 첫 번째 옵션을 기본 선택으로 삼고 대화창 출력
+        dialogueOptions = options;
+        isSelectingDialogueOption = true;
+        selectedDialogueOptionIndex = 0;
+        UpdateDialogueOptionText();
+        AdjustBackgroundSize();
+
+        // 입력 활성화하고 선택이 끝날 때까지 대기
+        UserInterfaceStack.PushUserInterface(this);
+        await UniTask.WaitUntil(() => !isSelectingDialogueOption);
+        UserInterfaceStack.PopUserInterface(this);
+
+        // 최종적으로 선택한 번호 반환
+        return selectedDialogueOptionIndex;
+    }
+
+    void IUserInterfaceController.BindInputActions()
+    {
+        InputManager.InputActions.UI.Up.performed += SelectUpperDialogueOption;
+        InputManager.InputActions.UI.Down.performed += SelectLowerDialogueOption;
+        InputManager.InputActions.UI.Select.performed += FinishSelectingDialogueOption;
+    }
+
+    void IUserInterfaceController.UnbindInputActions()
+    {
+        InputManager.InputActions.UI.Up.performed -= SelectUpperDialogueOption;
+        InputManager.InputActions.UI.Down.performed -= SelectLowerDialogueOption;
+        InputManager.InputActions.UI.Select.performed -= FinishSelectingDialogueOption;
+    }
+
+    public void SelectUpperDialogueOption(InputAction.CallbackContext context)
+    {
+        selectedDialogueOptionIndex = Mathf.Max(0, selectedDialogueOptionIndex - 1);
+        UpdateDialogueOptionText();
+    }
+
+    public void SelectLowerDialogueOption(InputAction.CallbackContext context)
+    {
+        selectedDialogueOptionIndex = Mathf.Min(dialogueOptions.Count - 1, selectedDialogueOptionIndex + 1);
+        UpdateDialogueOptionText();
+    }
+
+    public void FinishSelectingDialogueOption(InputAction.CallbackContext context)
+    {
+        isSelectingDialogueOption = false;
+    }
+
+    private void UpdateDialogueOptionText()
+    {
+        string newText = "";
+        for (int i = 0; i < dialogueOptions.Count; ++i)
+        {
+            // 현재 선택중인 옵션에만 밑줄 추가
+            if (i == selectedDialogueOptionIndex)
+            {
+                newText += $"<u>{dialogueOptions[i]}</u>";
+            }
+            else
+            {
+                newText += $"{dialogueOptions[i]}";
+            }
+
+            // 마지막 옵션이 아닌 경우 개행
+            if (i != dialogueOptions.Count - 1)
+            {
+                newText += "\n";
+            }
+        }
+
+        textMesh.text = newText;
     }
 }
