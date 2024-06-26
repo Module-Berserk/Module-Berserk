b56703b854a3ce9b115e8cb3dfc9fb40f904dca4
ownfos <mejhg1234@gmail.com>
2024-06-26 13:48:51 +0900

    Adjust dialogue box setting so that various horizontal alignments work

--- ModuleBerserk/Assets/Scripts/Test/DialogueBox.cs
+++ ModuleBerserk/Assets/Scripts/Test/DialogueBox.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using TMPro;
 using Cysharp.Threading.Tasks;
 using UnityEngine.InputSystem;
+using UnityEngine.Assertions;
 
 public class DialogueBox : MonoBehaviour, IUserInterfaceController
 {
@@ -20,24 +21,65 @@ public class DialogueBox : MonoBehaviour, IUserInterfaceController
 
     private void Start()
     {
-        TestDialogueSelection().Forget();
-        // TestDialogueSequence().Forget();
+        AssertConfiguration();
+
+        // TODO: 테스트 끝나면 삭제할 것
+        // TestDialogueSelection().Forget();
+        TestDialogueSequence().Forget();
+    }
+
+    // 정상적인 대화창 prefab 설정인지 확인
+    private void AssertConfiguration()
+    {
+        // 컴포넌트 레퍼런스 설정 완료
+        Assert.IsNotNull(backgroundRenderer);
+        Assert.IsNotNull(textMesh);
+
+        // 텍스트도 배경 이미지와 마찬가지로 중앙에 정렬되어야 함
+        // Note: 배경 이미지는 9-sliced 방식이라 자동으로 가로/세로 중앙 정렬임
+        Assert.IsTrue(textMesh.verticalAlignment == VerticalAlignmentOptions.Middle);
+        Assert.IsTrue(Mathf.Approximately(textMesh.rectTransform.pivot.x, 0.5f));
+        Assert.IsTrue(Mathf.Approximately(textMesh.rectTransform.localPosition.x, 0f));
+
+        // 직접 개행 문자를 넣지 않는 이상 대화창이 가로로 쭉 늘어남
+        Assert.IsFalse(textMesh.enableWordWrapping);
+        Assert.IsTrue(textMesh.overflowMode == TextOverflowModes.Overflow);
+    }
+
+    public void ShowBox()
+    {
+        backgroundRenderer.enabled = true;
+        textMesh.enabled = true;
+    }
+
+    public void HideBox()
+    {
+        backgroundRenderer.enabled = false;
+        textMesh.enabled = false;
     }
 
     private async UniTask TestDialogueSelection()
     {
-        List<string> options = new();
-        options.Add("1. 커피를 마신다");
-        options.Add("2. 도넛을 먹는다");
-        options.Add("3. 굶는다");
+        ShowBox();
+
+        List<string> options = new()
+        {
+            "1. 커피를 마신다",
+            "2. 도넛을 먹는다",
+            "3. 굶는다"
+        };
 
         int result = await BeginDialogueSelection(options);
+
+        HideBox();
 
         Debug.Log($"최종 선택: {options[result]}");
     }
 
     private async UniTask TestDialogueSequence()
     {
+        ShowBox();
+
         await BeginTypingAnimation("하늘을 올려다봐도 하늘밖에 없다");
         await UniTask.WaitForSeconds(1f);
         await BeginTypingAnimation("강은 바다로 넓어지고 사람은 죽음으로 넘치네");
@@ -45,12 +87,15 @@ public class DialogueBox : MonoBehaviour, IUserInterfaceController
         await BeginTypingAnimation("추운 겨울에는 <u>따뜻한 커피</u>와 티를 마셔야지요");
         await UniTask.WaitForSeconds(1f);
         await BeginTypingAnimation("사막에서 걷는 법을 알려줘\n신발에 모래가 들어가서 발을 델 것 같아");
+        await UniTask.WaitForSeconds(1f);
+
+        HideBox();
     }
 
-    private async UniTask BeginTypingAnimation(string dialogue)
+    public async UniTask BeginTypingAnimation(string dialogue)
     {
         textMesh.text = dialogue;
-        AdjustBackgroundSize();
+        AdjustBoxSize();
 
         // 아무 글자도 보이지 않는 상태에서 한 글자씩 출력
         textMesh.maxVisibleCharacters = 0;
@@ -64,16 +109,19 @@ public class DialogueBox : MonoBehaviour, IUserInterfaceController
     }
 
     // 전체 텍스트가 들어갈 수 있도록 배경 이미지의 사이즈를 조정
-    private void AdjustBackgroundSize()
+    private void AdjustBoxSize()
     {
         // 1. 전체 텍스트의 mesh를 준비한다
         textMesh.maxVisibleCharacters = textMesh.text.Length;
         textMesh.ForceMeshUpdate();
 
-        // 2. mesh의 크기를 기준으로 약간의 padding을 더해 배경 이미지 크기를 조정한다
+        // 2. mesh에 맞게 text 오브젝트의 rectTransform size를 변경한다 (width, height)
+        textMesh.rectTransform.sizeDelta = new Vector2(textMesh.renderedWidth, textMesh.renderedHeight);
+
+        // 3. mesh의 크기를 기준으로 약간의 padding을 더해 배경 이미지 크기를 조정한다
         backgroundRenderer.size = new Vector2(textMesh.renderedWidth + widthPadding, textMesh.renderedHeight + heightPadding);
 
-        // 3. 텍스트와 배경 이미지 모두 중앙에 정렬되므로
+        // 4. 배경 이미지는 중앙에 정렬되므로
         // 말풍선 꼬리가 제자리에 머물도록 약간 오른쪽으로 중심을 옮긴다
         transform.localPosition = backgroundRenderer.size / 2f + Vector2.up * heightOffset;
     }
@@ -85,7 +133,7 @@ public class DialogueBox : MonoBehaviour, IUserInterfaceController
         isSelectingDialogueOption = true;
         selectedDialogueOptionIndex = 0;
         UpdateDialogueOptionText();
-        AdjustBackgroundSize();
+        AdjustBoxSize();
 
         // 입력 활성화하고 선택이 끝날 때까지 대기
         UserInterfaceStack.PushUserInterface(this);
