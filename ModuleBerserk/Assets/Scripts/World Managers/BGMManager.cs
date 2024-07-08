946947adcdc72f7e63770a9775edb80762211bad
ownfos <mejhg1234@gmail.com>
2024-07-08 16:13:55 +0900

    Implement BGMManager

--- ModuleBerserk/Assets/Scripts/World Managers/BGMManager.cs
+++ ModuleBerserk/Assets/Scripts/World Managers/BGMManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-public class C1BGMManager : MonoBehaviour
+public class BGMManager : MonoBehaviour
 {
     [SerializeField]
     [Range(0, 100)]
