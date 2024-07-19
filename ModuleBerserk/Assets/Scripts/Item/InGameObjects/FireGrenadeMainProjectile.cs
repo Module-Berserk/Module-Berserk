f3039dc5015378543ee5ad2e62c0827c2b76514b
ownfos <mejhg1234@gmail.com>
2024-07-19 14:51:04 +0900

    Prevent fire grenade from exploding on walls and roofs

--- ModuleBerserk/Assets/Scripts/Item/InGameObjects/FireGrenadeMainProjectile.cs
+++ ModuleBerserk/Assets/Scripts/Item/InGameObjects/FireGrenadeMainProjectile.cs
@@ -12,8 +12,14 @@ public class FireGrenadeMainProjectile : MonoBehaviour
 
     private bool isProjectileSpawned = false;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionStay2D(Collision2D other)
     {
+        // 바닥이 아니라 벽/천장에는 반응하지 않음
+        if (Vector2.Dot(other.contacts[0].normal, Vector2.up) < 0.1f)
+        {
+            return;
+        }
+
         // 경사로에서 둘 이상의 콜라이더에 동시에 충돌하는
         // 상황에서도 한 번만 파편을 생성하도록 제한함.
         if (!isProjectileSpawned)
