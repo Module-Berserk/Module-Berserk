e96803f6d6e7ea73c93c692ceb589a317e9b14ae
ownfos <mejhg1234@gmail.com>
2024-08-09 23:06:20 +0900

    Implement cyclic motion of convoyer belt

--- ModuleBerserk/Assets/Scripts/Environment/ConvoyerBelt.cs
+++ ModuleBerserk/Assets/Scripts/Environment/ConvoyerBelt.cs
@@ -1,36 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
-using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Localization.PropertyVariants.TrackedObjects;
 
-[RequireComponent(typeof(CapsuleCollider2D))]
 public class ConvoyerBelt : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D movingPlatform;
+    [Header("Platform Movement")]
+    // 한 바퀴 도는데 걸리는 시간
+    [SerializeField] private float singleCycleDuration = 5f;
+    [SerializeField] private Ease platformMovementEase = Ease.InOutSine;
 
-    private CapsuleCollider2D convoyerBeltBoundary;
+
+    [Header("Automatic Loops")]
+    // 활성화하면 자동으로 무한 왕복을 시작함
+    [SerializeField] private bool autoLoop = false;
+
+
+    [Header("Component References (DO NOT MODIFY)")]
+    [SerializeField] private Rigidbody2D movingPlatform;
+    [SerializeField] private CapsuleCollider2D convoyerBeltBoundary;
+
+
+    private Vector2[] path;
 
     private void Awake()
     {
-        convoyerBeltBoundary = GetComponent<CapsuleCollider2D>();
-    }
-
-    public void Start()
-    {
-        var boundary = GetComponent<CapsuleCollider2D>();
-        Vector2 center = boundary.transform.position;
-        Vector2 right = boundary.transform.right;
-        Vector2 up = boundary.transform.up;
-        Vector2 halfSize = boundary.size / 2f;
+        Vector2 center = convoyerBeltBoundary.transform.position;
+        Vector2 right = convoyerBeltBoundary.transform.right;
+        Vector2 up = convoyerBeltBoundary.transform.up;
+        Vector2 halfSize = convoyerBeltBoundary.size / 2f;
 
         float radius = halfSize.y;
         float width = halfSize.x - halfSize.y;
 
         float sin45 = Mathf.Sqrt(2f) / 2;
 
-        Vector2[] path =
+        path = new[]
         {
             center - right * (width + radius),
             center - right * (width + radius * sin45) + up * radius * sin45,
@@ -39,9 +42,36 @@ public class ConvoyerBelt : MonoBehaviour
             center + right * width + up * radius,
             center + right * (width + radius * sin45) + up * radius * sin45,
             center + right * (width + radius),
+            center + right * (width + radius * sin45) - up * radius * sin45,
+            center + right * width - up * radius,
+            center - up * radius,
+            center - right * width - up * radius,
+            center - right * (width + radius * sin45) - up * radius * sin45,
+            center - right * (width + radius),
         };
         movingPlatform.position = path[0];
-        movingPlatform.DOPath(path, 5f, PathType.CatmullRom, PathMode.Sidescroller2D, 10, gizmoColor: Color.red)
-        .SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+    }
+
+    private void OnDestroy()
+    {
+        movingPlatform.DOKill();
+    }
+
+    private void Start()
+    {
+        if (autoLoop)
+        {
+            movingPlatform.DOPath(path, singleCycleDuration, PathType.CatmullRom, PathMode.Sidescroller2D)
+                .SetLoops(-1, LoopType.Restart)
+                .SetEase(Ease.Linear)
+                .SetUpdate(UpdateType.Fixed);
+        }
+    }
+
+    public void MoveSingleCycle()
+    {
+        movingPlatform.DOPath(path, singleCycleDuration, PathType.CatmullRom, PathMode.Sidescroller2D)
+            .SetEase(platformMovementEase)
+            .SetUpdate(UpdateType.Fixed);
     }
 }
